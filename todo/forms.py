aa0e09fc927c811d9044cc739a6fde29baa490d7
tsedenbaljir <tsedenbaljire@gmail.com>
2020-03-07 22:06:26 +0800

    #44

--- todo/forms.py
+++ todo/forms.py
@@ -1,16 +1,26 @@
-from django.core.exceptions import NON_FIELD_ERRORS
 from django import forms 
-from .models import Bed
+from .models import *
 
 class NameForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(NameForm, self).__init__(*args, **kwargs)
-        self.fields['kind'].required = False
-        self.fields['current'].required = False
-        self.fields['increment'].required = False
-        self.fields['decrement'].required = False
-        self.fields['final'].required = False
-
+        for k,f in self.fields.items():
+            if 'invalid' in f.error_messages:
+                self.fields[k].error_messages['invalid'] = 'Буруу формат байна.'
+            if 'required' in f.error_messages:
+                self.fields[k].error_messages['required'] = 'Бөглөх шаардлагатай.'
+            if 'max_length' in f.error_messages:
+                self.fields[k].error_messages['max_length'] = 'Хэт урт байна.'
+            if 'min_length' in f.error_messages:
+                self.fields[k].error_messages['min_length'] = 'Хэт богино байна.'
+        
+        for key, field in self.fields.items():
+            print(field.widget)
+            if isinstance(field.widget, forms.Select):
+                field.widget.attrs.update({'placeholder': "songolt xiine vv"})
+            if isinstance(field.widget, forms.NumberInput):
+                field.widget.attrs.update({'placeholder': "Зөвхөн тоо "})
+        
     class Meta:
         model=Bed
         fields=['kind','current','increment','decrement','final'] 
@@ -21,4 +31,3 @@ class NameForm(forms.ModelForm):
             "decrement": "Санал болгож буй орны бууралт",
             "final": "Төсөл дууссаны дараах нийт ор",
         }
- 
--- todo/views.py
+++ todo/views.py
@@ -1,32 +1,16 @@
 from django.shortcuts import render, redirect, HttpResponse
 from .models import Bed
 from .forms import NameForm
-
 # 
 def add(request): 
-    kinderr=""
-    currenterr=""
-    incrementerr=""
-    decrementerr=""
-    finalerr=""
     if request.method == 'POST':
-        form = NameForm()
-        if request.POST['kind'] != '' and request.POST['current'] != '' and request.POST['increment'] != '' and request.POST['decrement'] != '' and request.POST['final'] != '':
-            add=NameForm(request.POST)
-            if add.is_valid():
-                add.save()
-                return render(request, 'add.html', {'form':form,'saved':'Амжилттай хадгалгадлаа.'})
-        if request.POST['kind'] == '': 
-            kinderr="Сонголт хийнэ үү."
-        if isinstance(request.POST['current'],str) :
-            currenterr="Тоо оруулана уу"
-        if isinstance(request.POST['increment'],str) :
-            incrementerr="Тоо оруулана уу"
-        if isinstance(request.POST['decrement'],str) :
-            decrementerr="Тоо оруулана уу"
-        if isinstance(request.POST['final'],str) :
-            finalerr="Тоо оруулана уу" 
+        add=NameForm(request.POST)
+        if add.is_valid():
+            add.save()
+            form = NameForm()
+            return render(request, 'add.html', {'form':form,'saved':'Амжилттай хадгалгадлаа.'})
+        else:
+            form = add
     else:
         form = NameForm()
-
-    return render(request, 'add.html', {'form':form,'kinderr':kinderr,'currenterr':currenterr,'incrementerr':incrementerr,'decrementerr':decrementerr,'finalerr':finalerr})
+    return render(request, 'add.html', {'form':form})
